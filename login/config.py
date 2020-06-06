0faaccdd2dab24129c2cdbddb3ca48cf6db5ceb1
ayushzenith <ayushp.zenith@gmail.com>
2020-06-06 15:40:57 -0400

    Fixed database bug

--- login/config.py
+++ login/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config(object):
     SECRET_KEY = "secret"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///"+os.getcwd()+"/login/app.db"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///"+os.getcwd()+"/login/users.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     TWITTER_OAUTH_CLIENT_KEY = ""
     TWITTER_OAUTH_CLIENT_SECRET = ""
